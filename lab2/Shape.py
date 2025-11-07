bc72ae597f1f59e0ad0c993171cc1b3e7ca3f764
kaka <mrkaktail@gmail.com>
2025-11-07 16:37:10 +0100

    t

--- lab2/Shape.py
+++ lab2/Shape.py
@@ -4,31 +4,22 @@ class Shape:
             raise ValueError("x and y must be numbers")
         self.x = x
         self.y = y
-        print(f"created shape on x:{self.x}y:{self.y}")
+        print(f"created shape on x:{self.x} y:{self.y}")
     
-    def translate(self, xpath, ypath):
-        if not isinstance(xpath, (float, int)) or not isinstance(ypath, (float, int)): # if input is not a number
-            raise TypeError("x (x-path) and y (y-path) must be numbers")
-        self.x = xpath
-        self.y = ypath
+    def translate(self, xstep, ystep):
+        if not isinstance(xstep, (float, int)) or not isinstance(ystep, (float, int)): # if input is not a number
+            raise TypeError("x(x-path) and y(y-path) must be numbers")
+        self.x += xstep
+        self.y += ystep
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(x:{self.x}, y:{self.y})"
     
     def __str__(self):
-        return f"{self.__class__.__name__} centered at x: {self.x} and y: {self.y}  " #str repr
-    
-    def __repr__(self):
-        return f"{self.__class__.__name__}({self.x}, {self.y})" #str repr
+        return f"{self.__class__.__name__} positioned at x:{self.x}, y:{self.y}"
 
-#def __lt__(self, other): #     < #area and we will not have area
-    
-def __gt__(self, other): #     >
-    return self == other or self > other
-def __le__(self, other): #    <=
-    return not self <= other
-def __ge__(self,other): #     >=
-    return not self >= other 
-
-# def __eq__(self, other): return self == other #cant have yet if we compare area(each shape different)
-
+#circle1 = Shape(3, 5)
+#print(circle1)
 
 
 
