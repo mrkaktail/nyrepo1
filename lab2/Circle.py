e81aba2c9c8cabefca7241a36fea736439ec3350
kaka <mrkaktail@gmail.com>
2025-11-07 17:49:25 +0100

    commit 2

--- lab2/Circle.py
+++ lab2/Circle.py
@@ -3,12 +3,13 @@ from math import pi
 
 class Circle(Shape):
     def __init__(self, radius, x=0, y=0):
-        super.__init__(Circle.x,Circle.y)
+        super().__init__(x,y)
         if not isinstance(radius, (float, int)):
             raise ValueError("radius must be a number")
         self.radius = radius
-        return f"Circle with radius:{self.radius}"
+        return None
 
+    
     @property
     def area(self):
         return pi * (self.radius ** 2)
@@ -16,5 +17,31 @@ class Circle(Shape):
     @property
     def perimeter(self):
         return self.radius * pi * 2
+    
+    
+    def __eq__(self, other):
+        if not isinstance(other, Circle):
+            return False
+        return self.area == other.area                  #i could basically compare them as circ1.area == circ2.area without the method because its just two numbers genreally
+    
+    def __lt__(self, other):
+        if not isinstance(other, Circle):
+            raise TypeError("only areas of circles can be compared")
+        return self.area < other.area
+    
+    def __gt__(self, other):
+        if not isinstance(other, Circle):
+            raise TypeError("only areas of circles can be compared")
+        return self.area > other.area
+    def __le__(self, other):
+        if not isinstance(other, Circle):
+            raise TypeError("only areas of circles can be compared")
+        return self.area <= other.area
+    def __ge__(self, other):
+        if not isinstance(other, Circle):
+            raise TypeError("only areas of circles can be compared")
+        return self.area >= other.area 
+    
+#circ1 = Circle(5, 2, 6)
 
-circ1 = Circle(5, 2, 6)
+#circ1
